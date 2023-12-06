0726135d050b42fba9ac51a5c1cc8e3d76a39232
Matheus Puerari Trevisan <matheus15694@gmail.com>
2023-12-06 18:33:45 -0300

    menu hamb

--- assets/js/menuHamb.js
+++ assets/js/menuHamb.js
@@ -1,17 +1,18 @@
 document.querySelector('#menu-bar').addEventListener ('click', () => {
     document.querySelector('#menu-bar').classList.toggle("change");
-    document.querySelector('#nav').classList.toggle("change");
-    document.querySelector('#menu-bg').classList.toggle("change-bg");
+    document.querySelector('.nav').classList.toggle("change");
+    document.querySelector('.menu-bg').classList.toggle("change-bg");
     document.querySelector('.overlay').classList.toggle("hidden");document.querySelector('header').classList.toggle("change");
 })
 
 document.querySelector('.overlay').addEventListener ('click', () => {
     document.querySelector('#menu-bar').classList.toggle("change");
-    document.querySelector('#nav').classList.toggle("change");
-    document.querySelector('#menu-bg').classList.toggle("change-bg");
+    document.querySelector('.nav').classList.toggle("change");
+    document.querySelector('.menu-bg').classList.toggle("change-bg");
     document.querySelector('.overlay').classList.toggle("hidden");
     document.querySelector('header').classList.toggle("change");
 })
+
 
 
 
